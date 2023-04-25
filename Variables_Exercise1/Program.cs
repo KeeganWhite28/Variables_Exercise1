561ca7172e5b271902270500c282a9efa3cc3d33
Keegan White <keegan28white@gmail.com>
2023-04-25 13:19:23 -0500

    initial commit

---
+++ Variables_Exercise1/Program.cs
@@ -0,0 +1,14 @@
+﻿string name = "Keegan";
+int age = 24;
+char middleInitial = 'A';
+bool isOver18 = true;
+double currentTemp = 46;
+decimal currentPrice = 29.99m;
+
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"Is Over 18: {isOver18}");
+Console.WriteLine($"Current Temp: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
+               
